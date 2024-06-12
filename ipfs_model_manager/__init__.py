978f251c496e594df7e4eac8cdcc58e15af4d1d0
endomorphosis <starworks5@gmail.com>
2024-06-12 00:43:33 -0700

    chore: Update IPFS and OrbitDB imports in ipfs_model_manager

--- ipfs_model_manager/__init__.py
+++ ipfs_model_manager/__init__.py
@@ -1,8 +1,6 @@
 
-# from .ipfs_kit_lib import ipfs_kit
-# from .ipfs_model_manager import ipfs_model_manager
-# from .s3_kit import s3_kit
-# from .test import test_fio
-# from .test import test_hf_ipfs
-# from .orbitdb_kit_lib import orbitdb_kit_lib
-# from config import config 
+from .s3_kit import s3_kit
+from .test import test_fio
+from .test import test_hf_ipfs
+from .config import config 
+from .aria2 import install_aria2
